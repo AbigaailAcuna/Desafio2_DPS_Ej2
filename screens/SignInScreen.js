71317451b35253364ae0f62f8647bef1c87cedef
AbigaailAcuna <acuaabigail@yahoo.com>
2023-09-28 14:01:55 -0600

    Funcionamiento de login y registro

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,86 +1,86 @@
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native'
+// SignInScreen.js
+
 import React, { useState } from 'react';
-import CustomInput from '../components/Input'
-import Button from '../components/Button'
-
-
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CustomInput from '../components/Input';
+import Button from '../components/Button';
 
 const SignInScreen = () => {
-    const [correo, setCorreo] = useState('');
-    const [contra, setContra] = useState('');
-    const [isContraVisible, setContraVisible] = useState(false);
+  const [correo, setCorreo] = useState('');
+  const [contra, setContra] = useState('');
+  const [isContraVisible, setContraVisible] = useState(false);
 
+  const LogInPress = async () => {
+    try {
+      // Obtener las credenciales almacenadas
+      const storedCorreo = await AsyncStorage.getItem('correo');
+      const storedContra = await AsyncStorage.getItem('contra');
 
-    const SignInPress = () => {
-        console.warn("Registrarse");
+      // Verificar las credenciales ingresadas
+      if (correo === storedCorreo && contra === storedContra) {
+        // Las credenciales son correctas, permitir el acceso a la aplicación
+        console.warn('Inicio de sesión exitoso');
+        // Aquí puedes navegar a la pantalla principal de tu aplicación
+      } else {
+        // Las credenciales son incorrectas, mostrar un mensaje de error
+        console.warn('Credenciales incorrectas');
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión: ', error);
     }
+  };
 
-    const LogInPress = () => {
-        console.warn("Login");
-    }
+  const ContraVisibility = () => {
+    setContraVisible(!isContraVisible);
+    console.warn('Se cambia el estado de la contraseña');
+  };
 
-    const ContraVisibility = () => {
-        setContraVisible(!isContraVisible);
-        console.warn("Se cambia el estado de la contraseña")
-    }
+  const RegistroPress = () =>{
+    console.warn("registro");
+  }
+  return (
+    <ScrollView>
+      <View style={styles.root}>
+        <Text style={styles.texto}>Login</Text>
+        <CustomInput
+          placeholder="Correo electrónico"
+          value={correo}
+          setValue={setCorreo}
+          icononame="envelope"
+        />
 
-    return (
-        <ScrollView>
-            <View style={styles.root}>
-                <Text style={styles.texto}>Login</Text>
-                <CustomInput
-                    placeholder="Correo electrónico"
-                    value={correo}
-                    setValue={setCorreo}
-                    icononame="envelope"
-                />
+        <CustomInput
+          placeholder="Contraseña"
+          value={contra}
+          setValue={setContra}
+          secureTextEntry={!isContraVisible}
+          icononame="lock"
+          iconpassword={isContraVisible ? 'eye' : 'eye-slash'}
+          onPress={ContraVisibility}
+        />
 
-                <CustomInput
-                    placeholder="Contraseña"
-                    value={contra}
-                    setValue={setContra}
-                    secureTextEntry={!isContraVisible}
-                    icononame="lock" 
-                    iconpassword={isContraVisible ? 'eye' : 'eye-slash'}
-                    onPress={ContraVisibility}/>
-               
-                <Button
-                    text="Login"
-                    onPress={LogInPress}
-                    type="PRIMARY"
-                    size={350}/>
+        <Button text="Login" onPress={LogInPress} type="PRIMARY" size={350} />
 
-                <Button
-                    text=" ¿No tienes cuenta? Registrate"
-                    onPress={SignInPress} type="TERTIARY" />
-
-              
-            </View>
-        </ScrollView>
-    )
-}
+        <Button text="¿No tienes cuenta? Registrate" onPress={RegistroPress} type="TERTIARY" />
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center',
-        justifyContent: 'flex-start', //la sube
-        padding: 20,
-        height: '100%',
-        marginTop: 150,
-    },
-    texto: {
-        fontSize: 35,
-        fontWeight: 'bold',
-        marginBottom:40,
-    },
-    texto3: {
-        color: '#2C2B2D',
-        fontSize: 16,
-        marginBottom: 10,
-        marginTop: 20,
-
-    },
-
+  root: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 20,
+    height: '100%',
+    marginTop: 150,
+  },
+  texto: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
 });
 
 export default SignInScreen;
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,105 +1,112 @@
-import { View, Text, Image, StyleSheet, ScrollView, TextInput } from 'react-native'
 import React, { useState } from 'react';
-import CustomInput from '../components/Input'
-import Button from '../components/Button'
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CustomInput from '../components/Input';
+import Button from '../components/Button';
 import CheckBox from '@react-native-community/checkbox';
 
-
-
 const Login = () => {
-    {/*capturando valores*/ }
-    const [correo, setCorreo] = useState('');
-    const [contra, setContra] = useState('');
-    const [nombre, setnombre] = useState('');
-    const [contra2, setContra2] = useState('');
-    const [aceptaTerminos, setAceptaTerminos] = useState(false);
+  const [correo, setCorreo] = useState('');
+  const [contra, setContra] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [contra2, setContra2] = useState('');
+  const [aceptaTerminos, setAceptaTerminos] = useState(false);
+  const [isContraVisible, setContraVisible] = useState(false);
 
-    const [isContraVisible, setContraVisible] = useState(false);
+  const RegistroPress = async () => {
+    try {
+      // Verifica si el usuario ha aceptado los términos y condiciones
+      if (!aceptaTerminos) {
+        console.warn('Debes aceptar los términos y condiciones');
+        return;
+      }
 
+      // Aquí puedes realizar la validación de los campos antes de guardar las credenciales
 
-    {/*eventos botones */ }
-    const RegistroPress = () => {
-        console.warn("hola");
+      // Guarda las credenciales en AsyncStorage
+      await AsyncStorage.setItem('correo', correo);
+      await AsyncStorage.setItem('contra', contra);
+
+      // Muestra un mensaje de registro exitoso
+      console.warn('Registro exitoso');
+      // Aquí puedes navegar a la pantalla de inicio de sesión (SignInScreen) o realizar alguna otra acción.
+    } catch (error) {
+      console.error('Error al registrarse: ', error);
     }
+  };
 
-    const SingInPress = () => {
-        console.warn("hola");
-    }
+  const ContraVisibility = () => {
+    setContraVisible(!isContraVisible);
+    console.warn('Se cambia el estado de la contraseña');
+  };
 
-    const ContraVisibility = () => {
-        setContraVisible(!isContraVisible);
-        console.warn("Se cambia el estado de la contraseña")
-    }
+  return (
+    <ScrollView>
+      <View style={styles.root}>
+        <Text style={styles.texto}>Registrarse</Text>
 
-    return (
-        <ScrollView>
-            <View style={styles.root}>
-                <Text style={styles.texto}>Registrarse</Text>
+        <CustomInput
+          placeholder="Ingresa tu Nombre"
+          value={nombre}
+          setValue={setNombre}
+        />
+        <CustomInput
+          placeholder="Ingresa tu correo electrónico"
+          value={correo}
+          setValue={setCorreo}
+        />
+        <CustomInput
+          placeholder="Contraseña"
+          value={contra}
+          setValue={setContra}
+          secureTextEntry={!isContraVisible}
+          iconpassword={isContraVisible ? 'eye' : 'eye-slash'}
+          onPress={ContraVisibility}
+        />
 
-                <CustomInput
-                    placeholder="Ingresa tu Nombre"
-                    value={nombre}
-                    setValue={setnombre}
+        <CustomInput
+          placeholder="Repetir Contraseña"
+          value={contra2}
+          setValue={setContra2}
+          secureTextEntry={!isContraVisible}
+          iconpassword={isContraVisible ? 'eye' : 'eye-slash'}
+          onPress={ContraVisibility}
+        />
 
-                />
-                <CustomInput
-                    placeholder="Ingresa tu correo electrónico"
-                    value={correo}
-                    setValue={setCorreo}
-
-                />
-                <CustomInput
-                    placeholder="Contraseña"
-                    value={contra}
-                    setValue={setContra}
-                    secureTextEntry={!isContraVisible}
-                    iconpassword={isContraVisible ? 'eye' : 'eye-slash'}
-                    onPress={ContraVisibility} />
-
-                <CustomInput
-                    placeholder="Repetir Contraseña"
-                    value={contra2}
-                    setValue={setContra2}
-                    secureTextEntry={!isContraVisible}
-                    iconpassword={isContraVisible ? 'eye' : 'eye-slash'}
-                    onPress={ContraVisibility} />
-
-                <View style={{ flexDirection: 'row', alignItems: 'center' , marginTop:30}}>
-                    <CheckBox
-                        value={aceptaTerminos}
-                        onValueChange={(newValue) => setAceptaTerminos(newValue)}
-                    />
-                    <Text style={{ marginLeft: 8 }}>
-                        Acepto los <Text style={{ color: 'blue' }}>términos y condiciones</Text>
-                    </Text>
-                </View>
-                <Button
-                    text="Registrarse"
-                    onPress={RegistroPress}
-                    type="PRIMARY"
-                    size={350} />
-
-
-            </View>
-        </ScrollView>
-    )
-}
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 30 }}>
+          <CheckBox
+            value={aceptaTerminos}
+            onValueChange={(newValue) => setAceptaTerminos(newValue)}
+          />
+          <Text style={{ marginLeft: 8 }}>
+            Acepto los <Text style={{ color: 'blue' }}>términos y condiciones</Text>
+          </Text>
+        </View>
+        <Button
+          text="Registrarse"
+          onPress={RegistroPress}
+          type="PRIMARY"
+          size={350} />
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    root: {
-        justifyContent: 'flex-start', //la sube
-        padding: 20,
-        height: '100%',
-        marginTop: 25,
-    },
-    texto: {
-        fontSize: 35,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 40
-    },
-
-
+  root: {
+    justifyContent: 'flex-start',
+    padding: 20,
+    height: '100%',
+    marginTop: 25,
+  },
+  texto: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 40
+  },
 });
 
 export default Login;
+
+   
